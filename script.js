b2556d496c6658213d033e3c69c42c18a011800a
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2024-11-03 20:10:45 +0400

    Finished

---
+++ script.js
@@ -0,0 +1,29 @@
+let number = 2;
+console.log(number * number);
+
+
+if (number * number > 100) {
+  console.log(`Its more than a hunder`);
+} else if (number * number < 100) {
+    console.log(`Its less than a hundred`);
+}
+
+
+
+let number1 = 57;
+
+if ( 100 > number > 50) {
+console.log(`Its more than a 50 and less then a 100`);    
+} else {
+    console.log(`Doesnt match`);
+}
+
+let number2 = 0;
+
+if (number2 > 0) {
+    console.log(`Positive`);
+} else if (number2 == 0){
+    console.log(`Equals 0`);
+} else if (number2 < 0) {
+    console.log(`Negative`);
+}
